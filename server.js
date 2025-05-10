b342fec4c5d5eab5689027f3d53b33d1f6986b0b
Michael Mwanza <mwanza.n.m@gmail.com>
2025-05-11 00:22:52 +0200

    port edit

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require('express');
 const app = express();
 const mongodb = require('./data/database.js');
 
-const port = process.env.PORT || 8080;
+const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
